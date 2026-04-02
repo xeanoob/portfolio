1732af0b45a00f480e36bbf47740a983895617e5
xeanoo <boutrinambroise@gmail.com>
2026-04-02 13:18:02 +0200

    amélioration seo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: '*',
             allow: '/',
-            disallow: '/private/',
+            disallow: ['/private/', '/*?q=*'],
         },
         sitemap: 'https://ambroise-boutrin.fr/sitemap.xml',
     }
