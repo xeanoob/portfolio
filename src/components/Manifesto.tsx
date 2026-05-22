ef93275498d4aa054383de861dbf6c4034caf08a
xeanoo <boutrinambroise@gmail.com>
2026-05-22 15:13:19 +0200

    correction manifesto

--- src/components/Manifesto.tsx
+++ src/components/Manifesto.tsx
@@ -10,8 +10,9 @@ export default function Manifesto() {
 
             {/* Background Texture over the whole section */}
             <div className="absolute inset-0 z-0">
-                <div className="absolute inset-0 opacity-20 mix-blend-soft-light" style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.7' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E")` }}></div>
+                <div className="absolute inset-0 bg-[url('https://grainy-gradients.vercel.app/noise.svg')] opacity-20 mix-blend-soft-light"></div>
             </div>
+
 
             <div className="container mx-auto px-6 relative z-10">
 
